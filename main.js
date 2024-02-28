069d6b663819c77e1ea49753d4b363b427a83661
Matthew Haynes <124mhaynes@frhsd.com>
2024-02-28 07:47:29 -0500

    lesson 2 completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            description: 'They are socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://vuemastery.com/'
         }
