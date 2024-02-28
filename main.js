f88d3ab044b7f1267d9430de4e6a89723ee76c93
Matthew Haynes <124mhaynes@frhsd.com>
2024-02-28 11:32:08 -0500

    lesson 7  completed

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            product: 'Socks',
-            description: 'They are socks',
+    data () {
+        return{
+            cart:0,
+            product: "Socks",
+            description: "They are socks",
             image: './assets/images/socks_green.jpg',
-            url: 'https://vuemastery.com/',
-            inventory: 100,
+            url: "https://www.vuemastery.com",
+            inventory: 0,
             onSale: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            details: ["50 cotton", "30% wool", "20% polyester"],
+            variants:[
+                { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg"},
+                { id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg"}
             ],
-            sizes: ['xsmall', 'small', 'medium', 'large', 'x-large'
-            ],
-            data(){
-                return{
-                    cart:1
-                }
-            },
-            methods: {
-
-            addToCart(){
-                this.cart += 1
-            },
-            removeFromCart(){
-                if (this.cart >= 1){
-                    this.cart -= 1
-                }
-            },
-            updateImage(variantImage){
-                this.image = variantImage
-            }
-            }
+            sizes: ["xsmall", "small", "medium", "large", "xlarge"],
         }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1;
+        },
+        updateImage(variantImage){
+            this.image = variantImage;
+        },
+        removeFromCart(){
+            if (this.cart>0){
+                this.cart -=1;
+            } else{
+                this.cart = 0;
+            }
+           
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        }
+    },
+    inStock() {
+        return this.inventory > 0
     }
 })
