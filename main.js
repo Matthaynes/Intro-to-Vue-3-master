21fc070262ad33b8e70523b4ca3dabac8175287a
Matthew Haynes <124mhaynes@frhsd.com>
2024-02-28 08:24:37 -0500

    lesson 6  completed

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'They are socks',
             image: './assets/images/socks_green.jpg',
@@ -9,11 +10,30 @@ const app = Vue.createApp({
             onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'}
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
             ],
             sizes: ['xsmall', 'small', 'medium', 'large', 'x-large'
-            ]
+            ],
+            data(){
+                return{
+                    cart:1
+                }
+            },
+            methods: {
+
+            addToCart(){
+                this.cart += 1
+            },
+            removeFromCart(){
+                if (this.cart >= 1){
+                    this.cart -= 1
+                }
+            },
+            updateImage(variantImage){
+                this.image = variantImage
+            }
+            }
         }
     }
 })
